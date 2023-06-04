ec7ac3822aebe7f6c69690ebf2cad8c3402518f9
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-06-04 14:32:36 -0400

    script updates

---
+++ jeff-ssh.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+if ! ( ip address | grep -q 169.254.10.10 ) ; then
+  sudo ip address add 169.254.10.10/16 broadcast + dev enp7s0u1u2
+fi
+
+HOST=169.254.100.2
+HOST=$(lanipof '00:1e:a6:00:63:22')
+
+echo "HOST=$HOST"
+
+exec ssh -i /j/ident/azure_glacial_inferno_jeffrey jeffrey@$HOST
+
+
