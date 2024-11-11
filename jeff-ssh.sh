baeebb80369fcc13c3942c62903c07761ef8f157
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-11-11 16:12:29 -0500

    added dynamic ETH_DEV which kind of will match my thunderbolt ethernet port

--- jeff-ssh.sh
+++ jeff-ssh.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+if [ -z "$ETH_DEV" ]; then
+  ETH_DEV=$(ip a | grep ': enp' | tail -1 | cut -d':' -f2 | tr -d '[:space:]')
+fi
+echo "ETH_DEV=$ETH_DEV"
+
 if ! ( ip address | grep -q 169.254.10.10 ) ; then
-  sudo ip address add 169.254.10.10/16 broadcast + dev enp7s0u1u2
+  sudo ip address add 169.254.10.10/16 broadcast + dev $ETH_DEV
 fi
 
 HOST=169.254.100.2
@@ -11,6 +16,11 @@ HOST=169.254.100.2
 
 echo "HOST=$HOST"
 
-exec ssh -i /j/ident/azure_glacial_inferno_jeffrey jeffrey@$HOST
+echo "Forwarding 127.0.0.1:9000"
+
+exec ssh \
+  -i /j/ident/azure_glacial_inferno_jeffrey \
+  -L 127.0.0.1:9000:127.0.0.1:9000 \
+   jeffrey@$HOST
 
 
